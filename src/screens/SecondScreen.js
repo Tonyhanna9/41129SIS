e18a70dd39b6b8d0aaa696f549e7ed51ada9fc05
BuildTools <unconfigured@null.spigotmc.org>
2022-09-06 18:06:59 +1000

    Function to upload pictures to Firebase.
    Function works but does not use a unique ID when uploading pictures, so
    files are being overwritten. Testing using the uuid and
    react-native-uuid packages so far yields no results.
    
    Firebase is currently setup to give everyone read-write permission to
    storage. Changes to this permission will break the code.

--- src/screens/SecondScreen.js
+++ src/screens/SecondScreen.js
@@ -1,16 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import {
   Layout,
   TopNav,
   Text,
+  Button,
   themeColor,
   useTheme,
 } from "react-native-rapi-ui";
 import { Ionicons } from "@expo/vector-icons";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import * as ImagePicker from "expo-image-picker";
+// import uuid from "react-native-uuid"
 
 export default function ({ navigation }) {
   const { isDarkmode, setTheme } = useTheme();
+
+  const [ image, setImage ] = useState(null);
+  const [ uploading, setUploading ] = useState(false);
+
+  async function useCamera() {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3]
+    });
+    console.log("Picture was taken using camera");
+
+    manageUpload(result);
+  }
+
+  async function useFile() {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3]
+    });
+    console.log("File selected: " + result);
+
+    manageUpload(result);
+  }
+
+  async function manageUpload(result) {
+    try {
+      setUploading(true);
+      
+      if (!result.cancelled) {
+        const imageUrl = await uploadImage(result.uri);
+        setImage(imageUrl);
+        console.log("Success");
+      }
+    } catch (e) {
+      console.log(e)
+      alert("Error when uploading image.");
+    } finally {
+      setUploading(false);
+    }
+  }
+
+  /**
+   * Reason to use below approach: https://github.com/expo/expo/issues/2402#issuecomment-443726662
+   */
+  async function uploadImage(uri) {
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+  
+    // Example uses uuid.v4() to create a unique name. The uuid and
+    // react-native-uuid packages have compile issues. 
+    const fileRef = ref(getStorage(), "images/Temp.png");
+    const result = await uploadBytes(fileRef, blob);
+
+    blob.close();
+  
+    return await getDownloadURL(fileRef);
+  }
+
   return (
     <Layout>
       <TopNav
@@ -45,8 +118,8 @@ export default function ({ navigation }) {
           justifyContent: "center",
         }}
       >
-        {/* This text using ubuntu font */}
-        <Text fontWeight="bold">This is the second screen</Text>
+        <Button text="1" onPress={useFile} />
+        <Button text="2" onPress={useCamera} />
       </View>
     </Layout>
   );
