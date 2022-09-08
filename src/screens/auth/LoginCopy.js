46e3b5094098df8cf4e3344d11418e22775d57a3
sukhpreetp <sukhpreet123@outlook.com>
2022-09-08 21:49:54 +1000

    Creating Home Page
    Draft UI - Work in progress

---
+++ src/screens/auth/LoginCopy.js
@@ -0,0 +1,173 @@
+import React, { useState } from "react";
+import {
+  ScrollView,
+  TouchableOpacity,
+  View,
+  KeyboardAvoidingView,
+  Image,
+} from "react-native";
+import {
+  Layout,
+  Text,
+  Button,
+  useTheme,
+  themeColor,
+} from "react-native-rapi-ui";
+
+export default function ({ navigation }) {
+  const { isDarkmode, setTheme } = useTheme();
+
+  return (
+    <KeyboardAvoidingView behavior="height" enabled style={{ flex: 1 }}>
+      <Layout
+        >
+        <ScrollView
+          contentContainerStyle={{
+            flexGrow: 1,
+          }}
+        >
+           <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: isDarkmode ? "#17171E" : themeColor.danger600,
+            }}
+          >
+            <Image
+              resizeMode="cover"
+              style={{
+                height: 220,
+                width: 220,
+              }}
+              source={require("../../../assets/FireArm.png")}
+            />
+          </View>
+          <View
+            style={{
+              flex: 3,
+              paddingHorizontal: 20,
+              paddingBottom: 20,
+              backgroundColor: isDarkmode ? themeColor.dark : themeColor.white,
+              borderRadius: 25
+            }}
+          >
+            <Text
+              fontWeight="bold"
+              style={{
+                alignSelf: "center",
+                padding: 30,
+              }}
+              size="h2"
+            >
+              Fire Emergency
+            </Text>
+            <Text
+            style={{
+              alignSelf: "center",
+            }}
+            >Click below to take a snapshot and report to authorities instantly</Text>
+            <Text></Text>
+            <Text></Text>
+            <Button
+              text={"REPORT NOW!"}
+              status="danger"
+            />
+
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: 15,
+                justifyContent: "center",
+              }}
+            >
+              <Text size="md"
+              style={{
+                alignSelf: "center",
+              }}
+              >Create an account to send your details along with the report and see real-time updates on fires near you</Text>
+              
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: 15,
+                justifyContent: "center",
+              }}
+            >
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("Register");
+                }}
+              >
+                <Text
+                  size="md"
+                  fontWeight="bold"
+                  style={{
+                    marginLeft: 5,
+                    color: "#ff4500",
+                  }}
+                >
+                  Create an account
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: 15,
+                justifyContent: "center",
+              }}
+            >
+              <Text size="md">Already have an account?</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("Login");
+                }}
+              >
+                <Text
+                  size="md"
+                  fontWeight="bold"
+                  style={{
+                    marginLeft: 5,
+                    color: "#ff4500",
+                  }}
+                >
+                  Login
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: 30,
+                justifyContent: "center",
+              }}
+            >
+              <TouchableOpacity
+                onPress={() => {
+                  isDarkmode ? setTheme("light") : setTheme("dark");
+                }}
+              >
+                <Text
+                  size="md"
+                  fontWeight="bold"
+                  style={{
+                    marginLeft: 5,
+                  }}
+                >
+                  {isDarkmode ? "☀️ light theme" : "🌑 dark theme"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      </Layout>
+    </KeyboardAvoidingView>
+  );
+}
