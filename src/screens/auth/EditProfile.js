24ee6ad8d9c7995883842df10afdb51814cf289c
Danny-Vien <79494703+Danny-Vien@users.noreply.github.com>
2022-09-09 17:30:54 +1000

    Added EditProfile.js

---
+++ src/screens/auth/EditProfile.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import {
+  ScrollView,
+  TouchableOpacity,
+  View,
+  KeyboardAvoidingView,
+  Image,
+} from "react-native";
+import { getAuth,  } from "firebase/auth";
+import {
+    Layout,
+    Text,
+    TextInput,
+    Button,
+    useTheme,
+    themeColor,
+  } from "react-native-rapi-ui";
+  import { db } from "../../config/firebase";
+  export default function ({ navigation }) {
+    const auth = getAuth();
+  }
