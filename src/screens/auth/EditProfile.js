24e03b7de17e0772a7a7b64d6f0312094467ed01
Danny-Vien <79494703+Danny-Vien@users.noreply.github.com>
2022-09-12 03:19:05 +1000

    Remove trial function

--- src/screens/auth/EditProfile.js
+++ src/screens/auth/EditProfile.js
@@ -17,15 +17,6 @@ import {
   } from "react-native-rapi-ui";
   import { db } from "../../config/firebase";
   export default function ({ navigation }) {
-    //const auth = getAuth();
+    const auth = getAuth();
     const userId = firebase.auth().currentUser.uid;
-    const userDocument = firestore()
-    .collection('Users')
-    .doc(userId)
-    .then(docSnapshot => {
-        if (docSnapshot.exists) {
-            const userData = docSnapshot.data()
-            console.log(userData)
-        }
-    });
   }
