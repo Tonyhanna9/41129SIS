9afa1f3e53824a3a14f60d80e991221458f2362c
emaangohar01@gmail.com <emaangohar01@gmail.com>
2022-10-16 22:53:51 +1100

    Fire Tracker/ Map implemented (screens not connected to app)

--- src/screens/map.js
+++ src/screens/map.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Linking } from "react-native";
 import { getAuth, signOut } from "firebase/auth";
+import react, { useState, useEffect } from "react";
 import {
   Layout,
   Button,
@@ -15,16 +16,69 @@ import { Ionicons } from "@expo/vector-icons";
 import {
   withGoogleMap,
   withScriptjs,
-  GoogleMap
+  GoogleMap,
+  Marker,
+  InfoWindow
 } from "react-google-maps";
 
+import * as fireData from "./fire.json";
+import mapStyles from "./mapStyles";
+
 function Map(){
+  const [setFire, setSelectedFire] = useState(null);
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedFire(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
   return( 
   
   <GoogleMap 
     defaultZoom={10} 
     defaultCenter = {{lat: 45.421532, lng: -75.697189 }} 
-    />
+    defaultOptions={{ styles: mapStyles }}
+    >
+
+{fireData.features.map(fire => (
+        <Marker
+          key={fire.properties.FIRE_ID}
+          position={{
+            lat: fire.geometry.coordinates[1],
+            lng: fire.geometry.coordinates[0]
+          }}
+           onClick={() => {
+             setSelectedFire(fire);
+           }}
+        />
+      ))} 
+
+      {setFire && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedFire(null);
+          }}
+          position={{
+            lat: setFire.geometry.coordinates[1],
+            lng: setFire.geometry.coordinates[0]
+          }}
+        >
+          <div>
+            <h2>{setFire.properties.NAME}</h2>
+            <p>{setFire.properties.DESCRIPTIO}</p>
+          </div>
+        </InfoWindow>
+      )}
+
+  </GoogleMap>
 
   );
 }
@@ -37,7 +91,7 @@ export default function ({ navigation }) {
   return (
     <Layout>
       <TopNav
-        middleContent="Home"
+        middleContent="Fire Tracker"
         rightContent={
           <Ionicons
             name={isDarkmode ? "sunny" : "moon"}
